f219f4e1f1822313c480961182a8f92c2dc49b88
BernardoAgrelos <bernardo.agrelos@edu.unirio.br>
2023-06-25 20:24:14 -0300

    Ultima esperaça

--- Aluguel.API/ViewModels/PassaporteViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Aluguel.API.ViewModels
-{
-    public class PassaporteViewModel
-    {
-        public string? Numero { get; set; }
-        public string? Validade { get; set; }
-        public string? Pais { get; set; }
-    }
-}
