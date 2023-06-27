45e0a4d293c85dfd582fde29a1a13820f0d45259
AllanisC <allanis@edu.unirio.br>
2023-06-27 20:02:41 -0300

    testes

--- Aluguel.Tests/CiclistaControllerTest.cs
+++ Aluguel.Tests/CiclistaControllerTest.cs
@@ -1,95 +1,100 @@
-//using Aluguel.API.AutoMapperProfiles;
-//using Aluguel.API.Controllers;
-//using Aluguel.API.Services;
-//using Aluguel.API.ViewModels;
-//using Aluguel.Tests.MockData;
-//using AutoMapper;
-//using FluentAssertions;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Extensions.Logging;
-//using Moq;
-//using Xunit;
+using Aluguel.API.AutoMapperProfiles;
+using Aluguel.API.Controllers;
+using Aluguel.API.Services;
+using Aluguel.API.ViewModels;
+using Aluguel.Tests.MockData;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
 
-//namespace Aluguel.Tests;
+namespace Aluguel.Tests;
 
-//public class CiclistaControllerTest
-//{
-//    [Fact]
-//    //public void CreateOnSuccessReturnStatusCode200()
-//    //{
-//    //    var mockMapper = new Mock<IMapper>();
-//    //    var mockLogger = new Mock<ILogger<CiclistaController>>();
-//    //    var mockService = new Mock<ICiclistaService>();
+public class CiclistaControllerTest
+{
+    private readonly Mock<ILogger<CiclistaController>> _logger = new();
+    [Fact]
+    public void CreateOnSuccessReturnStatusCode200()
+    {
+        var myProfile = new CiclistaAutoMapperProfile();
+        var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+        var mapper = new Mapper(configuration);
 
-//    //    var sut = new CiclistaController(mockLogger.Object, mockMapper.Object, mockService.Object);
+        var alugueMockService = new Mock<IAluguelService>();
+        var ciclistaMockService = new Mock<ICiclistaService>();
+        ciclistaMockService.Setup(service => service.CreateCiclista(CiclistaMockData.GetCiclistaMockData())).Returns(new CiclistaViewModel());
 
-//    //    var result = (OkObjectResult)sut.Create(CiclistaMockData.GetCiclistaMockData());
+        var sut = new CiclistaController(_logger.Object, ciclistaMockService.Object, alugueMockService.Object);
 
-//    //    result.StatusCode.Should().Be(200);
-//    //}
+        var result = (OkObjectResult)sut.Create(CiclistaMockData.GetCiclistaMockData());
 
-//    [Fact]
-//    public void EditOnSuccessReturnStatusCode200()
-//    {
-//        var myProfile = new CiclistaAutoMapperProfile();
-//        var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
-//        var mapper = new Mapper(configuration);
+        result.StatusCode.Should().Be(200);
+    }
 
-//        var mockLogger = new Mock<ILogger<CiclistaController>>();
-//        var mockService = new Mock<ICiclistaService>();
+    //[Fact]
+    //public void EditOnSuccessReturnStatusCode200()
+    //{
+    //    var myProfile = new CiclistaAutoMapperProfile();
+    //    var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+    //    var mapper = new Mapper(configuration);
 
-//        mockService.Setup(service => service.GetCiclista()).Returns(CiclistaMockData.GetCiclistaViewModelMockData());
+    //    var mockLogger = new Mock<ILogger<CiclistaController>>();
+    //    var mockService = new Mock<ICiclistaService>();
 
-//        var sut = new CiclistaController(mockLogger.Object, mapper, mockService.Object);
+    //    mockService.Setup(service => service.GetCiclista()).Returns(CiclistaMockData.GetCiclistaViewModelMockData());
 
-//        var result = (OkObjectResult)sut.Edit(CiclistaMockData.GetCiclistaEditMockData(), 0);
+    //    var sut = new CiclistaController(mockLogger.Object, mapper, mockService.Object);
 
-//        result.StatusCode.Should().Be(200);
-//    }
+    //    var result = (OkObjectResult)sut.Edit(CiclistaMockData.GetCiclistaEditMockData(), 0);
 
-//    [Fact]
-//    public void EnableciclistaOnSuccessReturnStatusCode200()
-//    {
-//        var mockMapper = new Mock<IMapper>();
-//        var mockLogger = new Mock<ILogger<CiclistaController>>();
-//        var mockService = new Mock<ICiclistaService>();
-//        mockService.Setup(service => service.GetCiclista()).Returns(new CiclistaViewModel());
+    //    result.StatusCode.Should().Be(200);
+    //}
 
-//        var sut = new CiclistaController(mockLogger.Object, mockMapper.Object, mockService.Object);
+    //[Fact]
+    //public void EnableciclistaOnSuccessReturnStatusCode200()
+    //{
+    //    var mockMapper = new Mock<IMapper>();
+    //    var mockLogger = new Mock<ILogger<CiclistaController>>();
+    //    var mockService = new Mock<ICiclistaService>();
+    //    mockService.Setup(service => service.GetCiclista()).Returns(new CiclistaViewModel());
 
-//        var result = (OkObjectResult)sut.EnableCiclista("000", 0);
+    //    var sut = new CiclistaController(mockLogger.Object, mockMapper.Object, mockService.Object);
 
-//        result.StatusCode.Should().Be(200);
-//    }
+    //    var result = (OkObjectResult)sut.EnableCiclista("000", 0);
 
-//    [Fact]
-//    public void EditCartaoOnSucessReturnsStatusCode200() 
-//    {
-//        var myProfile = new CiclistaAutoMapperProfile();
-//        var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
-//        var mapper = new Mapper(configuration);
+    //    result.StatusCode.Should().Be(200);
+    //}
 
-//        var mockLogger = new Mock<ILogger<CiclistaController>>();
-//        var mockService = new Mock<ICiclistaService>();
+    //[Fact]
+    //public void EditCartaoOnSucessReturnsStatusCode200()
+    //{
+    //    var myProfile = new CiclistaAutoMapperProfile();
+    //    var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+    //    var mapper = new Mapper(configuration);
 
-//        mockService.Setup(service => service.GetCiclista()).Returns(CiclistaMockData.GetCiclistaViewModelMockData());
+    //    var mockLogger = new Mock<ILogger<CiclistaController>>();
+    //    var mockService = new Mock<ICiclistaService>();
 
-//        var sut = new CiclistaController(mockLogger.Object, mapper, mockService.Object);
+    //    mockService.Setup(service => service.GetCiclista()).Returns(CiclistaMockData.GetCiclistaViewModelMockData());
 
-//        var result = (OkObjectResult)sut.EditCartao(CiclistaMockData.GetMeiodeDePagamentoMockData(), 0);
+    //    var sut = new CiclistaController(mockLogger.Object, mapper, mockService.Object);
 
-//        result.StatusCode.Should().Be(200);
+    //    var result = (OkObjectResult)sut.EditCartao(CiclistaMockData.GetMeiodeDePagamentoMockData(), 0);
 
-//    }
+    //    result.StatusCode.Should().Be(200);
 
-//    [Fact]  
-//    public void CiclistaServiceGetCiclistaReturnsCiclistaViewModel()
-//    {
-//        var service = new CiclistaService();
+    //}
 
-//        var sut = service.GetCiclista();
+    //[Fact]
+    //public void CiclistaServiceGetCiclistaReturnsCiclistaViewModel()
+    //{
+    //    var service = new CiclistaService();
 
-//        Assert.IsType<CiclistaViewModel>(sut);  
-//    }
+    //    var sut = service.GetCiclista();
 
-//}
+    //    Assert.IsType<CiclistaViewModel>(sut);
+    //}
+
+}
--- Aluguel.Tests/FuncionarioControllerTest.cs
+++ Aluguel.Tests/FuncionarioControllerTest.cs
@@ -1,75 +1,75 @@
-using Aluguel.API.AutoMapperProfiles;
-using Aluguel.API.Controllers;
-using Aluguel.API.Services;
-using Aluguel.API.ViewModels;
-using Aluguel.Tests.MockData;
-using AutoMapper;
-using FluentAssertions;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Moq;
-using Xunit;
+//using Aluguel.API.AutoMapperProfiles;
+//using Aluguel.API.Controllers;
+//using Aluguel.API.Services;
+//using Aluguel.API.ViewModels;
+//using Aluguel.Tests.MockData;
+//using AutoMapper;
+//using FluentAssertions;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Logging;
+//using Moq;
+//using Xunit;
 
-namespace Aluguel.Tests;
+//namespace Aluguel.Tests;
 
-public class FuncionarioControllerTest
-{
-    [Fact]
-    public void CreateOnSuccessReturnStatusCode200()
-    {
-        var mockMapper = new Mock<IMapper>();
-        var mockLogger = new Mock<ILogger<FuncionarioController>>();
-        var mockService = new Mock<IFuncionarioService>();
+//public class FuncionarioControllerTest
+//{
+//    [Fact]
+//    public void CreateOnSuccessReturnStatusCode200()
+//    {
+//        var mockMapper = new Mock<IMapper>();
+//        var mockLogger = new Mock<ILogger<FuncionarioController>>();
+//        var mockService = new Mock<IFuncionarioService>();
 
-        var sut = new FuncionarioController(mockLogger.Object, mockMapper.Object, mockService.Object);
+//        var sut = new FuncionarioController(mockLogger.Object, mockMapper.Object, mockService.Object);
 
-        var result = (OkObjectResult)sut.Create(FuncionarioMockData.GetFuncionarioInsertMockData());
+//        var result = (OkObjectResult)sut.Create(FuncionarioMockData.GetFuncionarioInsertMockData());
 
-        result.StatusCode.Should().Be(200);
-    }
+//        result.StatusCode.Should().Be(200);
+//    }
 
-    [Fact]
-    public void DeleteOnSuccessReturnStatusCode200()
-    {
-        var mockMapper = new Mock<IMapper>();
-        var mockLogger = new Mock<ILogger<FuncionarioController>>();
-        var mockService = new Mock<IFuncionarioService>();
+//    [Fact]
+//    public void DeleteOnSuccessReturnStatusCode200()
+//    {
+//        var mockMapper = new Mock<IMapper>();
+//        var mockLogger = new Mock<ILogger<FuncionarioController>>();
+//        var mockService = new Mock<IFuncionarioService>();
 
-        mockService.Setup(service => service.GetFuncionario()).Returns(FuncionarioMockData.GetFuncionarioViewModelMockData());
+//        mockService.Setup(service => service.GetFuncionario()).Returns(FuncionarioMockData.GetFuncionarioViewModelMockData());
 
-        var sut = new FuncionarioController(mockLogger.Object, mockMapper.Object, mockService.Object);
+//        var sut = new FuncionarioController(mockLogger.Object, mockMapper.Object, mockService.Object);
 
-        var result = (OkObjectResult)sut.Delete(0);
+//        var result = (OkObjectResult)sut.Delete(0);
 
-        result.StatusCode.Should().Be(200);
-    }
+//        result.StatusCode.Should().Be(200);
+//    }
 
-    [Fact]
-    public void EditOnSuccessReturnsStatusCode200()
-    {
-        var mockLogger = new Mock<ILogger<FuncionarioController>>();
-        var mockService = new Mock<IFuncionarioService>();
+//    [Fact]
+//    public void EditOnSuccessReturnsStatusCode200()
+//    {
+//        var mockLogger = new Mock<ILogger<FuncionarioController>>();
+//        var mockService = new Mock<IFuncionarioService>();
 
-        var myProfile = new FuncionarioAutoMapperProfile();
-        var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
-        var mapper = new Mapper(configuration);
+//        var myProfile = new FuncionarioAutoMapperProfile();
+//        var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+//        var mapper = new Mapper(configuration);
 
-        mockService.Setup(service => service.GetFuncionario()).Returns(FuncionarioMockData.GetFuncionarioViewModelMockData());
+//        mockService.Setup(service => service.GetFuncionario()).Returns(FuncionarioMockData.GetFuncionarioViewModelMockData());
 
-        var sut = new FuncionarioController(mockLogger.Object, mapper, mockService.Object);
+//        var sut = new FuncionarioController(mockLogger.Object, mapper, mockService.Object);
 
-        var result = (OkObjectResult)sut.Edit(FuncionarioMockData.GetFuncionarioEditViewModelMockData(), 0);
+//        var result = (OkObjectResult)sut.Edit(FuncionarioMockData.GetFuncionarioEditViewModelMockData(), 0);
 
-        result.StatusCode.Should().Be(200);
-    }
+//        result.StatusCode.Should().Be(200);
+//    }
 
-    [Fact]
-    public void FuncionarioServiceGetFuncionarioReturnsFuncionarioViewModel()
-    {
-        var service = new FuncionarioService();
+//    [Fact]
+//    public void FuncionarioServiceGetFuncionarioReturnsFuncionarioViewModel()
+//    {
+//        var service = new FuncionarioService();
 
-        var sut = new FuncionarioService().GetFuncionario();
+//        var sut = new FuncionarioService().GetFuncionario();
 
-        Assert.IsType<FuncionarioViewModel>(sut);
-    }
-}
+//        Assert.IsType<FuncionarioViewModel>(sut);
+//    }
+//}
