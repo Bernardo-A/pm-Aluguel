40547e334b9645e6936544cf5b1c8a4862c99462
AllanisC <allanis@edu.unirio.br>
2023-07-19 22:02:54 -0300

    mais testes

--- Aluguel.Tests/FuncionarioServiceTest.cs
+++ Aluguel.Tests/FuncionarioServiceTest.cs
@@ -24,4 +24,32 @@ public class FuncionarioServiceTest
         var result = sut.CreateFuncionario(new FuncionarioInsertViewModel());
         Assert.Equal(typeof(FuncionarioInsertViewModel), result.GetType());
     }
+
+    [Fact]
+    public void UpdateOnSuccessReturnStatusCode200()
+    {
+        var sut = new FuncionarioService(_mapper.Object);
+
+        var result = sut.UpdateFuncionario(new FuncionarioEditViewModel(), 0);
+        Assert.Equal(typeof(FuncionarioEditViewModel), result.GetType());
+    }
+
+    [Fact]
+    public void GetOnSuccessReturnStatusCode200()
+    {
+        var sut = new FuncionarioService(_mapper.Object);
+
+        var result = sut.GetFuncionario(0);
+        Assert.Equal(typeof(FuncionarioViewModel), result.GetType());
+    }
+
+    [Fact]
+    public void DeleteOnSuccessReturnStatusCode200()
+    {
+        var sut = new FuncionarioService(_mapper.Object);
+
+        var result = sut.DeleteFuncionario(0);
+        Assert.Equal(typeof(FuncionarioEditViewModel), result.GetType());
+    }
+
 }
